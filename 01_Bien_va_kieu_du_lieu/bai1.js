89c34c7bd5df01ba628b7ef7efe926dea881772f
nhungbth2310 <nhung.bth@cs.ventures>
2024-07-30 21:25:12 +0700

    bai 1 and bai 2

---
+++ 01_Bien_va_kieu_du_lieu/bai1.js
@@ -0,0 +1,5 @@
+//cau1
+let age =10;
+console.log ('Toi moi '+ age);
+console.log ('Toi con qua tre de choi game')
+
---
+++ 01_Bien_va_kieu_du_lieu/bai2.js
@@ -0,0 +1,5 @@
+//Cau2
+let x = '*****';
+for (i=0;i <= x.length; i++){  
+    console.log(x); 
+}
