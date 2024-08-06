5590c8506bbaec4bf8cf81a0e4620f5cb76cc92b
nhungbth2310 <nhung.bth@cs.ventures>
2024-08-06 21:15:19 +0700

    updated bai6

--- 01_Bien_va_kieu_du_lieu/bai6.js
+++ 01_Bien_va_kieu_du_lieu/bai6.js
@@ -1,9 +1,33 @@
 // x: Int;
 // y: Float;
 // z: Char;
-let x = 10;
+let x = true;
 let y = 12.1;
 let z = 'aa';
-console.log(x, 'la so nguyen');
-console.log(y, 'la so thuc');
-console.log(z, ' la ky tu');
+// console.log(x, 'la so nguyen');
+// console.log(y, 'la so thuc');
+// console.log(z, ' la ky tu');
+
+function printTypeof(a) {
+    if (typeof (a) == 'number' && Math.floor(a) == a) {
+        console.log(a, ' is Interger');
+    }
+    else if (typeof (a) == 'string') {
+        console.log(a, 'is String');
+
+    } 
+    else if (typeof (a) == 'number') {
+        console.log(a, 'is Float');
+    }
+    else if (typeof (a) == 'boolean') {
+        console.log(a, 'is Boolean');
+    }
+    else{
+        console.log('Exception');
+    }
+    
+};
+
+printTypeof(x);
+printTypeof(y);
+printTypeof(z);
