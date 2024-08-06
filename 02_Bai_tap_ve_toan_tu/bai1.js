38ab071ad02dd4f94d5d6f9dfe626c4811631d2d
nhungbth2310 <nhung.bth@cs.ventures>
2024-08-06 20:52:14 +0700

    toantu_bai1

---
+++ 02_Bai_tap_ve_toan_tu/bai1.js
@@ -0,0 +1,12 @@
+let a = 1;
+let b = 4;
+
+let Tong = a+b;
+let Hieu = a-b;
+let Tich = a*b;
+let Thuong = a/b;
+
+console.log ('Tong cua',a,'&',b,'la',Tong);
+console.log ('Hieu cua',a,'&',b,'la',Hieu);
+console.log ('Tich cua',a,'&',b,'la',Tich);
+console.log ('Thuong cua',a,'&',b,'la',Thuong);
