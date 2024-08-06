d57422a0eee694dcec2bc412ad6b613c40255ade
nhungbth2310 <nhung.bth@cs.ventures>
2024-08-06 20:50:04 +0700

    update bai 5

--- 01_Bien_va_kieu_du_lieu/bai5.js
+++ 01_Bien_va_kieu_du_lieu/bai5.js
@@ -1,39 +1,29 @@
-let a = 2;
-let b = 56;
-let c = 20;
+let a = -1;
+let b = -1;
+let c = -2;
 
 // findMin(a,b,c);
 // findMid(a,b,c);
 // findMax(a,b,c); 
+
 function findMin(a, b, c) {
-    if (a < b && a < c) {
+    if (a <= b && a <= c) {
         return a;
     }
-    else if ( b <a && b < c){
+    else if ( b <= a && b <= c){
         return b;
     }
     else {
         return c;
 
     }
-        
-    };
-function findMid(a, b, c) {
-    if (a > b && a < c) {
-        return a;
-    }
-    else if (b > a && b < c){
-        return b;
-    }
-    else {
-        return c;
-    }
-};
+}
+
 function findMax(a, b, c) {
-    if (a>b && a >c){
+    if (a>=b && a >= c){
         return a;
     }
-    else if (b >a && b> c){
+    else if (b >= a && b>= c){
         return b;
     }
     else{
@@ -41,11 +31,25 @@ function findMax(a, b, c) {
     }
     
 };
+      
+function findMid(a, b, c) {
+    let min = findMin(a, b, c);
+    let max = findMax(a, b, c);
+    if(a> min && a<max){
+        return a;
+    }
+    else if ( b > min && b<max){
+        return b;
+    }
+    else {
+        return c;
+    }
+}
 
-let min = findMin(a, b, c);
 let mid = findMid(a, b, c);
+let min = findMin(a, b, c);
 let max = findMax(a, b, c);
 
-console.log('thu tu tang dan la',min , mid , max);
-console.log('Thu tu tang dan la',max, mid, min);
+console.log('Order by asc',min , mid , max);
+console.log('Order by desc',max, mid, min);
 
