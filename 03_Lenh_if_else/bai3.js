7693b1de15c2b1f96500294778f95f41c46201be
nhungbth2310 <nhung.bth@cs.ventures>
2024-08-13 21:39:06 +0700

    update condition

--- 03_Lenh_if_else/bai3.js
+++ 03_Lenh_if_else/bai3.js
@@ -1,16 +1,24 @@
+let a=5;
+let b=30;
+let c=4; 
+let d=12;
+let e=1;
 
-
-function findMax(a,b,c){
-    if(a>b && a>c){
-        return a;
+function findMax(a, b, c, d, e) {
+    let max = a;
+    if (max < b) {
+        max = b;
     }
-    else if ( b>a&&b>c){
-        return b;
+    if (max < c) {
+        max = c;
     }
-    else {
-        return c;
+    if (max < d) {
+        max = d;
     }
-};
-let max = findMax(1,12,12);
-console.log("MAX = ", max)
-
+    if (max < e) {
+        max = e
+    }
+    return max;
+}
+let MAX=findMax(a,b,c,d,e);
+console.log('Gia tri lon nhat la', MAX);
