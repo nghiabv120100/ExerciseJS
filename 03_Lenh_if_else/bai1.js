38b8472641cb74add5296b0f36e6f5d06a1f075d
nhungbth2310 <nhung.bth@cs.ventures>
2024-08-13 20:54:08 +0700

    if_else_bai1

---
+++ 03_Lenh_if_else/bai1.js
@@ -0,0 +1,14 @@
+let age= 16;
+if (age == 16){
+    console.log('Tuoi cua hoc sinh la',age);
+    console.log('Hoc sinh du tuoi vao lop 10');
+}
+else if (age < 16){
+    console.log('Tuoi cua hoc sinh la',age);
+    console.log('Hoc sinh khong du tuoi vao lop 10');
+
+}
+else {
+    console.log('Tuoi cua hoc sinh la',age);
+    console.log('Do tuoi vuot qua de vao lop 10');
+}
